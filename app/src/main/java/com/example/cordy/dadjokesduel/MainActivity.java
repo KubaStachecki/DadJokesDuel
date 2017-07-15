767b20fcceb61897d16dc258f3fbdcd73e052603
KubaStachecki <jakub.stachecki@gmail.com>
2017-07-15 17:44:17 +0200

    firebase database 2

--- app/src/main/java/com/example/cordy/dadjokesduel/MainActivity.java
+++ app/src/main/java/com/example/cordy/dadjokesduel/MainActivity.java
@@ -2,19 +2,28 @@ package com.example.cordy.dadjokesduel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.cordy.dadjokesduel.Model.Joke;
 import com.example.cordy.dadjokesduel.Utils.MainToFragmentUtils;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements MainToFragmentUtils {
+
+
+    List<Joke> jokeList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // sout("Cześć Kuba"); if(true){openBeerAction(new Beer beer);}
-
-        // CZESC JACEK
 
 //TODO - splashscreen na poczatek i start (jako dialog fragment - ze slajdami z instrukcja)
 
@@ -27,5 +36,41 @@ public class MainActivity extends AppCompatActivity implements MainToFragmentUti
         //TODO
 
 
+        jokeList = new ArrayList<>();
+        makeJokesList();
+
+
     }
+
+    public void makeJokesList() {
+
+        FirebaseDatabase fDatabase = FirebaseDatabase.getInstance();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+        DatabaseReference databaseReference = fDatabase.getReference();
+        databaseReference.child("Jokes").addValueEventListener(new com.google.firebase.database.ValueEventListener() {
+
+            @Override
+            public void onDataChange(com.google.firebase.database.DataSnapshot dataSnapshot) {
+
+                Log.d("TAAAAG", Long.toString(dataSnapshot.getChildrenCount()) + "UDALO SIE POLACZYC");
+
+                jokeList.clear();
+
+                for (com.google.firebase.database.DataSnapshot child : dataSnapshot.getChildren()) {
+
+                    Joke joke = child.getValue(Joke.class);
+                    jokeList.add(joke);
+                    Log.d("TAAAAG", jokeList.size() + "ROZMIAR LISTY");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
 }
