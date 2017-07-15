7693cbe2d20ccbf055ab68e36615fedce71ab6f8
KubaStachecki <jakub.stachecki@gmail.com>
2017-07-15 15:34:59 +0200

    main structure

---
+++ app/src/main/java/com/example/cordy/dadjokesduel/JokesGenerator.java
@@ -0,0 +1,10 @@
+package com.example.cordy.dadjokesduel;
+
+/**
+ * Created by cordy on 2017-07-15.
+ */
+
+public class JokesGenerator {
+
+    //TODO klasa na sciaganie pliku - odczyt z pliku i nadpisanie pliku - ta klasa wystawia dowcipy
+}
--- app/src/main/java/com/example/cordy/dadjokesduel/MainActivity.java
+++ app/src/main/java/com/example/cordy/dadjokesduel/MainActivity.java
@@ -14,6 +14,14 @@ public class MainActivity extends AppCompatActivity {
 
         // CZESC JACEK
 
+//TODO - splashscreen na poczatek i start (jako dialog fragment - ze slajdami z instrukcja)
+
+        //TODO - fragment wyswietlajacy dowcipy w dwoch polach tekstowych (rozna czcionka - pytanie odpowiedz)
+        //TODO - math random ktory losuje z listy obiekt typu suchar
+        // TODO - retrofit ktory sciaga plik json z sucharami - plik jest wczytywany do bazy
+        // TODO wczytywanie obiektow z pliku json
+        //TODO dialog fragment z wygrana - animacja koniec gry - ilosc uzytych dowcipow (dialog fragment)
+        //TODO 
 
 
     }
